b2531a4b93295c3189c8c6f4afd6dbe0488d57e2
Eden <edenshapiro@gmail.com>
2017-08-13 16:57:47 -0700

    Add small extra features

--- SuperCoolTipCalculator/ViewController.swift
+++ SuperCoolTipCalculator/ViewController.swift
@@ -12,7 +12,7 @@ import CoreLocation
 import Kanna
 import Alamofire
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, UITextFieldDelegate {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
@@ -20,6 +20,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var leaveYelpReviewButton: UIButton!
     @IBOutlet weak var incorrectLocationButton: UIButton!
     let defaults = UserDefaults.standard
+    let formatter = NumberFormatter()
     var tipPercentages: [Double]!
     var defaultSegment: Int!
     var locationManager: CLLocationManager!
@@ -31,9 +32,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpFormatterAndBillField()
+        checkDefaultsForValue()
+    }
+    
+    func setUpFormatterAndBillField(){
+        billField.delegate = self
+        billField.becomeFirstResponder()
+        billField.keyboardType = .numberPad
+        billField.textAlignment = .right
+        formatter.numberStyle = .currency
+        formatter.locale = Locale.current
+        formatter.currencySymbol = Locale.current.currencySymbol!
+        formatter.currencyCode = Locale.current.currencyCode!
+    }
+    
+    func checkDefaultsForValue(){
+        let currentTime = NSDate()
+        print("in checkDefaultsForValue")
+        if let lastUsed = defaults.object(forKey: "lastUsed") {
+            
+            let timeSinceLastUsed = currentTime.timeIntervalSinceReferenceDate - (lastUsed as! NSDate).timeIntervalSinceReferenceDate
+            print(timeSinceLastUsed)
+            if timeSinceLastUsed > 600 {
+                defaults.removeObject(forKey: "billField")
+                defaults.synchronize()
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        
+        // Set up segmentedcontrol
         if let defaultsArray = defaults.array(forKey: "tipPercentages"){
             tipPercentages = defaultsArray as! [Double]
             print("app has run before")
@@ -42,7 +72,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             defaults.set(tipPercentages, forKey: "tipPercentages")
             print("first app run ever")
         }
-        
         let segment1 = String(Int(tipPercentages[0]*100)) + "%"
         let segment2 = String(Int(tipPercentages[1]*100)) + "%"
         let segment3 = String(Int(tipPercentages[2]*100)) + "%"
@@ -57,8 +86,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
         tipControl.selectedSegmentIndex = defaultSegment
         
-        determineMyCurrentLocation()
+        // Check billField for prior value
+        if let billField = defaults.string(forKey: "billField"){
+            self.billField.text = billField
+            calculateTip(billField as AnyObject)
+        } else {
+            self.billField.placeholder = formatter.currencySymbol
+        }
         
+        // Determine current user location
+        determineMyCurrentLocation()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+        let lastUsed = NSDate()
+        defaults.set(lastUsed, forKey: "lastUsed")
+        if let billField = self.billField.text {
+            print(billField)
+            defaults.set(billField, forKey: "billField")
+        }
+        defaults.synchronize()
     }
 
 
@@ -86,7 +134,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         print("user latitude = \(userLocation!.coordinate.latitude)")
         print("user longitude = \(userLocation!.coordinate.longitude)")
         
-        
         if let userLocation = userLocation {
             
             let lat = userLocation.coordinate.latitude
@@ -111,25 +158,33 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.leaveYelpReviewButton.setTitle("Leave Yelp review for \(business.name!)", for: .normal)
         
     }
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Error \(error)")
     }
-
-
 
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
-    // Called when segmentedcontrol value changes
+    // Called when segmentedcontrol value changes or when bill textfield changed
     @IBAction func calculateTip(_ sender: AnyObject) {
+        print(" ")
+        print(billField.text ?? "help")
+        let text = billField.text?.replacingOccurrences(of: formatter.currencySymbol, with: "").replacingOccurrences(of: formatter.groupingSeparator, with: "").replacingOccurrences(of: formatter.decimalSeparator, with: "")
         
-        let bill = Double(billField.text!) ?? 0
+        print("text after calTip formatting: \(text)")
+        let double = Double(text!) ?? 0
+        print("number: \(double)")
+        print("number/100: \(double/100.0)")
+        let bill = Double(double/100.0)
+        print(bill)
+        
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
+        tipLabel.text = formatter.string(from: NSNumber(value: tip))!
+        totalLabel.text = formatter.string(from: NSNumber(value: total))!
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
         defaults.set(tipControl.selectedSegmentIndex, forKey: "defaultSegment")
         
         
@@ -141,6 +196,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         //            self.firstView.alpha = 1
         //            self.secondView.alpha = 0
         //        })
+    }
+    
+    @IBAction func textFieldChanged(_ sender: Any) {
+        let textField = sender as! UITextField
+        let text = textField.text?.replacingOccurrences(of: formatter.currencySymbol, with: "").replacingOccurrences(of: formatter.groupingSeparator, with: "").replacingOccurrences(of: formatter.decimalSeparator, with: "")
+        print("cleaned up text: \(text)")
+        textField.text = formatter.string(from: NSNumber(value: (Double(text!) ?? 0)/100.0))
+        print("newly reformated text: \(textField.text)")
+       
+
     }
     
     @IBAction func leaveYelpReviewButtonClicked(_ sender: Any) {
@@ -191,8 +256,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                                                  options: .caseInsensitive)
             guard let myMatch = regex.firstMatch(in: html, options: .withoutAnchoringBounds, range: NSRange(location: 0, length: html.characters.count))
                 else {
-                    print("errrr")
-                    return //"problemelemlem"
+                    print("Error in parseHTML")
+                    return
                 }
             let tmp = (html as NSString).substring(with: myMatch.range)
             print("Match: \(tmp)")
